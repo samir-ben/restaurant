86639a07c391eea01fbd873e93b5ac1d39858738
samir-ben <samir-69@hotmail.fr>
2018-12-12 02:18:01 +0100

    feat : dish-table component
    - create dish-table component
    - add this component in carte component
    - add style in dish-table component
    - add data in datasource of this component
    - add price column in html  of this component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,12 +7,13 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MainNavComponent } from './main-nav/main-nav.component';
 import { LayoutModule } from '@angular/cdk/layout';
 import { MatToolbarModule, MatButtonModule, MatSidenavModule, MatIconModule, MatListModule,
-         MatCardModule, MatGridListModule, MatMenuModule } from '@angular/material';
+         MatCardModule, MatGridListModule, MatMenuModule, MatTableModule, MatPaginatorModule, MatSortModule } from '@angular/material';
 import { HomeComponent } from './home/home.component';
 import { CarteComponent } from './carte/carte.component';
 import { ReserveComponent } from './reserve/reserve.component';
 import { ContactComponent } from './contact/contact.component';
 import { MainDashComponent } from './main-dash/main-dash.component';
+import { DishTableComponent } from './dish-table/dish-table.component';
 
 const routes: Routes = [
   { path: '', component: HomeComponent },
@@ -29,7 +30,8 @@ const routes: Routes = [
     CarteComponent,
     ReserveComponent,
     ContactComponent,
-    MainDashComponent
+    MainDashComponent,
+    DishTableComponent
   ],
   imports: [
     BrowserModule,
@@ -43,7 +45,10 @@ const routes: Routes = [
     MatCardModule,
     RouterModule.forRoot(routes),
     MatGridListModule,
-    MatMenuModule
+    MatMenuModule,
+    MatTableModule,
+    MatPaginatorModule,
+    MatSortModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/dish-table/dish-table.component.spec.ts
@@ -0,0 +1,32 @@
+import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { NoopAnimationsModule } from '@angular/platform-browser/animations';
+import { MatPaginatorModule, MatSortModule, MatTableModule } from '@angular/material';
+
+import { DishTableComponent } from './dish-table.component';
+
+describe('DishTableComponent', () => {
+  let component: DishTableComponent;
+  let fixture: ComponentFixture<DishTableComponent>;
+
+  beforeEach(async(() => {
+    TestBed.configureTestingModule({
+      declarations: [ DishTableComponent ],
+      imports: [
+        NoopAnimationsModule,
+        MatPaginatorModule,
+        MatSortModule,
+        MatTableModule,
+      ]
+    }).compileComponents();
+  }));
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(DishTableComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should compile', () => {
+    expect(component).toBeTruthy();
+  });
+});
