00708553a259f10a8af17f3653aa7ed9bacc0bc4
samir-ben <samir-69@hotmail.fr>
2018-12-13 16:06:10 +0100

    data : quick lunch service
    - import food interface
    - add data in quick-lunch service

--- src/app/services/quick-lunch.service.ts
+++ src/app/services/quick-lunch.service.ts
@@ -1,4 +1,5 @@
 import { Injectable } from '@angular/core';
+import { Food } from '../models/food.interface';
 
 @Injectable({
   providedIn: 'root'
@@ -6,4 +7,23 @@ import { Injectable } from '@angular/core';
 export class QuickLunchService {
 
   constructor() { }
+
+  tacos: Food[] = [
+    { id: 1, title: 'Uno', description: '1 viande, tomate, salade iceberg, oigons rouges, sauce maison', price: 5.50 },
+    { id: 2, title: 'Dos', description: '2 viandes, cheddar, tomate, salade iceberg, oignons rouges, sauce maison', price: 6.50 },
+    { id: 3, title: 'Tres', description: '3 viandes, cheddar, tomate, salade iceberg, oignons rouges, sauce maison', price: 7.50 }
+  ];
+  chips: Food[] = [
+    { id: 1, title: 'Barquette', description: 'Frittes maison', price: 3.50 },
+    { id: 2, title: 'Barquette & Frommage', description: 'Frittes maison plus sauche fromagère', price: 4.00 },
+    { id: 3, title: 'Chips', description: 'Assiète de chips épicés accompagnée du sauce maison', price: 3.00 },
+    { id: 4, title: 'Combo frittes & chips', description: 'Assiète de frittes et de chips épicés fait maison accompagnée du sauce maison',
+     price: 5.00 }
+  ];
+  coffees: Food[] = [
+    { id: 1, title: 'Expresso', price: 1.50 },
+    { id: 2, title: 'Cappuccino', price: 2.50 },
+    { id: 2, title: 'Mocha', price: 2.50 },
+    { id: 2, title: 'Café long', price: 1.00 }
+  ];
 }
