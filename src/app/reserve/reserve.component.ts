c1ef6df06d82a3d3fabed1c513888be9ab8dbce1
samir-ben <samir-69@hotmail.fr>
2018-12-14 22:10:07 +0100

    feat : form
    - add a customer form

--- src/app/reserve/reserve.component.ts
+++ src/app/reserve/reserve.component.ts
@@ -14,6 +14,13 @@ export class ReserveComponent implements OnInit {
   timeOptions = ['12:00', '12:30', '13:00', '13:30', '19:00', '19:30', '20:00', '20:30'];
   peopleOptions = [1, 2, 3, 4, 5, 6, 7, 8];
 
+  customer = {
+    firstName: '',
+    lastName: '',
+    email: '',
+    phone: ''
+  };
+
   constructor(private fb: FormBuilder) { }
 
   ngOnInit() {
@@ -26,6 +33,9 @@ export class ReserveComponent implements OnInit {
       time: '',
       people: 1
     });
+  }
+  finalizeReservation(value) {
+    console.log(value);
   }
 
 }
